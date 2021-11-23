29be324be4d65bfac379de7c13fb59c180631742
m.ganji1985@gmail.com <m.ganji1985@gmail.com>
2021-11-23 09:03:23 +0000

    final

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -11,19 +11,24 @@ orders as (
 
 ),
 
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
 customer_orders as (
 
     select
-        customer_id,
+        orders.customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders,
+        sum(payments.amount) as lifetime_value
     from orders
-
+    join payments using (order_id) 
     group by 1
-
 ),
 
 final as (
@@ -34,7 +39,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders, 
+        customer_orders.lifetime_value 
 
     from customers
 
