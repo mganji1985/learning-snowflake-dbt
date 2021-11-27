3b17046ef27166012209587dfecf1b72e6921727
m.ganji1985@gmail.com <m.ganji1985@gmail.com>
2021-11-27 12:07:02 +0000

    ninja for pivoted reports

--- models/marts/paymrnts_pivoted.sql
+++ models/marts/paymrnts_pivoted.sql
@@ -1,10 +1,12 @@
 select
 orderid,
-sum ( case when paymentmethod='credit_card' then amount else 0 end ) as credit_card_amount ,
-sum ( case when paymentmethod='coupon' then amount else 0 end ) as coupon_amount,
-sum ( case when paymentmethod='gift_card' then amount else 0 end ) as gift_card_amount, 
-sum ( case when paymentmethod='bank_transfer' then amount else 0 end ) as bank_transfer_amount 
-
+{% set payment_method=['credit_card', 'coupon', 'gift_card', 'bank_transfer' ] -%}
+{%-   for method in payment_method -%} 
+   sum ( case when paymentmethod='{{ method}}' then amount else 0 end ) as {{ method}}_amount 
+   {%- if not loop.last -%}
+      ,
+   {% endif %}
+ {% endfor%}
  from  stripe.payment
 
  group by 1
